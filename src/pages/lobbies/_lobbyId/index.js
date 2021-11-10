5019babdb30095f9150bee2c0c62681c258b35f7
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-11-10 13:30:22 -0500

    fetch game and redirect to create lobby by game

--- src/pages/lobbies/_lobbyId/index.js
+++ src/pages/lobbies/_lobbyId/index.js
@@ -3,9 +3,9 @@ import { SpinLoaderMin } from "../../../components/common/loader";
 import { useRouter } from "next/router";
 import { firestore } from "../../../firebase";
 
-export const Lobby = () => {
+export const Lobby = (props) => {
   const router = useRouter();
-  const { gameId, userId } = router.query;
+  const { gameId, userId, lobbyId } = router.query;
 
   useEffect(() => {
     if (!gameId) return router.push("/");
@@ -14,11 +14,14 @@ export const Lobby = () => {
       // TODO: All games created on BIngo or Hanged should be clone to bombo-games
       const gameQuery = await firestore.doc(`games/${gameId}`).get();
 
-      if (gameQuery.exists) return router.push("/");
+      if (!gameQuery.exists) {
+        props.showNotification("ERROR", "Juego no encontrado!");
+        return router.push("/");
+      }
 
       const currentGame = gameQuery.data();
 
-      await router.push(`/${currentGame?.name?.toUpperCase()}/lobby/new?gameId=${gameId}&userId=${userId}`);
+      await router.push(`/${currentGame?.name?.toUpperCase()}/lobbies/${lobbyId}?gameId=${gameId}&userId=${userId}`);
     };
 
     fetchGame();
