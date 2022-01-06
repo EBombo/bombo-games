84b690b8578427321b874b8c169016c3b10fb6f1
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2022-01-06 16:35:40 -0500

    headers & chunk bundle

--- next.config.js
+++ next.config.js
@@ -11,17 +11,64 @@ module.exports = withBundleAnalyzer({
       })
     );
 
+    config.optimization = {
+      sideEffects: true,
+      runtimeChunk: "single",
+      minimize: true,
+      minimizer: [],
+      splitChunks: {
+        chunks: "all",
+        maxInitialRequests: Infinity,
+        minSize: 200000,
+        maxSize: 250000,
+      },
+    };
+
     return config;
   },
-  async rewrites() {
+  async headers() {
     return [
       {
-        source: "/bingo/:path*",
-        destination: "https://bingo-red.ebombo.io/bingo/:path*",
+        source: "/:all*(jpg|jpeg|gif|png|svg|ico)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2592000",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
       },
       {
-        source: "/hanged/:path*",
-        destination: "https://hanged-red.ebombo.io/hanged/:path*",
+        source: "/:all*(eot|otf|ttf|ttc|woff|font.css)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2592000",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
+      },
+      {
+        source: "/:all*(js|css|json)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=2592000",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+        ],
       },
     ];
   },
