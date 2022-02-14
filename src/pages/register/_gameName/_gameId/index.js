300a363206cb0156e97cdf3071927ff135799544
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2022-02-14 17:06:59 -0500

    removing extra parts

--- src/pages/register/_gameName/_gameId/index.js
+++ src/pages/register/_gameName/_gameId/index.js
@@ -1,12 +1,9 @@
 import React from "reactn";
 import UserLayout from "../../../../components/UserLayout";
-import { Image } from "../../../../components/common/Image";
-import { config, firestoreBingo, firestoreRoulette } from "../../../../firebase";
+import { firestoreBingo, firestoreRoulette } from "../../../../firebase";
 import { object, string } from "yup";
 import { useForm } from "react-hook-form";
 import { ButtonAnt, Input } from "../../../../components/form";
-import { ArrowRightOutlined } from "@ant-design/icons";
-import { darkTheme } from "../../../../theme";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import { useSendError } from "../../../../hooks";
@@ -21,7 +18,6 @@ export const Register = (props) => {
 
   const validationSchema = object().shape({
     name: string().required(),
-    cardId: string().required(),
     email: string().required().email(),
   });
 
@@ -84,13 +80,6 @@ export const Register = (props) => {
           <div className="text-['Lato'] text-[30px] leading-[36px] font-[900] text-whiteDark my-4">
             Link de inscripción
           </div>
-          <Image
-            src={`${config.storageUrl}/resources/companies/carvajal.png`}
-            width="225px"
-            height="auto"
-            size="contain"
-            margin="0"
-          />
         </div>
 
         <div className="w-full rounded-[10px] bg-whiteDark p-4">
@@ -134,45 +123,6 @@ export const Register = (props) => {
                   border="0"
                   placeholder="Escribe tu correo aquí..."
                 />
-              </div>
-            </div>
-
-            <div className="w-full rounded-[4px] bg-whiteLight p-4 my-4">
-              <div className="grid gap-[1rem] md:grid-cols-[2fr_1fr]">
-                <div>
-                  <div className="text-secondary mb-4 text-['Lato'] text-[20px] leading-[24px] font-bold">
-                    ¿Cuál es el ID de tu cartilla?
-                  </div>
-                  <div className="text-['Lato'] font-[400] text-[16px] leading-[20px] text-grayLight my-4">
-                    (Se encuentra en la parte superior izquierda de tu cartilla)
-                  </div>
-                  <div className="flex flex-col	md:flex-row">
-                    <Input
-                      ref={register}
-                      error={errors.cardId}
-                      name="cardId"
-                      height="65px"
-                      background="#E3E3E3"
-                      border="0"
-                      placeholder="Escribe el ID aquí..."
-                    />
-                  </div>
-                </div>
-                <div className="flex m-auto">
-                  <ArrowRightOutlined style={{ fontSize: "16px", color: darkTheme.basic.secondary }} />
-                  <div className="ml-[10px]">
-                    <div className="text-['Lato'] font-bold text-[12px] leading-[14px] text-grayLight">ID 001</div>
-                    <div>
-                      <Image
-                        src={`${config.storageUrl}/resources/bingo-card.png`}
-                        width="115px"
-                        height="145px"
-                        size="contain"
-                        margin="0"
-                      />
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
 
