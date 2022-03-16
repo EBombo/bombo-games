89ebc5404e1a01d3ce18bc9312eecf690a1bdbc9
Pablo <pablo.yncio@immertec.com>
2022-03-16 14:43:29 -0500

    merge with red

--- src/pages/register/_gameName/_gameId/index.js
+++ src/pages/register/_gameName/_gameId/index.js
@@ -1,7 +1,7 @@
 import React from "reactn";
 import UserLayout from "../../../../components/UserLayout";
-import { firestoreBingo, firestoreRoulette } from "../../../../firebase";
-import { object, string } from "yup";
+import { firestoreBingo, firestoreHanged, firestoreRoulette, firestoreTrivia } from "../../../../firebase";
+import { boolean, object, string } from "yup";
 import { useForm } from "react-hook-form";
 import { ButtonAnt, Input } from "../../../../components/form";
 import { useRouter } from "next/router";
@@ -15,10 +15,12 @@ export const Register = (props) => {
   const { sendError } = useSendError();
 
   const [isLoading, setIsLoading] = useState(false);
+  const [inscription, setInscription] = useState(false);
 
   const validationSchema = object().shape({
     name: string().required(),
     email: string().required().email(),
+    attendance: boolean().required(),
   });
 
   const { register, errors, handleSubmit } = useForm({
@@ -38,7 +40,14 @@ export const Register = (props) => {
       setIsLoading(true);
 
       // Define firebase ref.
-      const firebaseRef = gameName === "bingo" ? firestoreBingo : firestoreRoulette;
+      const firebaseRef =
+        gameName === "bingo"
+          ? firestoreBingo
+          : gameName === "roulette"
+          ? firestoreRoulette
+          : gameName === "trivia"
+          ? firestoreTrivia
+          : firestoreHanged;
 
       const game = await fetchGame(firebaseRef);
 
@@ -51,8 +60,10 @@ export const Register = (props) => {
 
       await visitorRef.doc(visitorId).set({
         ...data,
+        id: visitorId,
         createAt: new Date(),
         updateAt: new Date(),
+        deleted: false,
         game: {
           id: gameId,
           name: game.name,
@@ -61,8 +72,7 @@ export const Register = (props) => {
         },
       });
 
-      props.showNotification("Congratulations!", "Se ha inscrito correctamente.", "success");
-      router.push("/");
+      setInscription(true);
     } catch (error) {
       console.error(error);
       await sendError(error);
@@ -70,6 +80,30 @@ export const Register = (props) => {
       setIsLoading(false);
     }
   };
+
+  if (inscription) {
+    return (
+      <div className="bg-secondary w-full h-screen bg-center bg-contain bg-pattern">
+        <UserLayout />
+        <div className="w-full p-4 max-w-[820px] m-auto">
+          <div className="flex flex-col-reverse items-center md:flex-row md:justify-between my-4 mx-auto">
+            <div className="text-['Lato'] text-[30px] leading-[36px] font-[900] text-whiteDark my-4">
+              Link de inscripción
+            </div>
+          </div>
+
+          <div className="w-full rounded-[10px] bg-whiteDark p-4">
+            <div className="text-secondary mb-4 text-['Lato'] text-[40px] leading-[48px] font-bold text-center">
+              ¡Muchas gracias!
+            </div>
+            <div className="text-secondary mb-4 text-['Lato'] text-[20px] leading-[24px] font-bold text-center">
+              Has llenado el formulario correctamente
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="bg-secondary w-full h-screen bg-center bg-contain bg-pattern">
@@ -92,7 +126,7 @@ export const Register = (props) => {
                   name="name"
                   height="65px"
                   background="#E3E3E3"
-                  placeholder="Nombres"
+                  placeholder="Nombre"
                   border="0"
                 />
 
@@ -103,7 +137,7 @@ export const Register = (props) => {
                   background="#E3E3E3"
                   height="65px"
                   border="0"
-                  placeholder="Apellidos"
+                  placeholder="Apellido"
                 />
               </div>
             </div>
@@ -122,6 +156,49 @@ export const Register = (props) => {
                   border="0"
                   placeholder="Escribe tu correo aquí..."
                 />
+              </div>
+            </div>
+
+            <div className="w-full rounded-[4px] bg-whiteLight p-4 my-4">
+              <div className="text-secondary mb-4 text-['Lato'] text-[20px] leading-[24px] font-bold">
+                ¿Asistirás al evento?
+              </div>
+              <div className="flex flex-col">
+                <div className="flex items-center gap-[10px] mb-4">
+                  <input
+                    type="radio"
+                    ref={register}
+                    error={errors.attendance}
+                    id="attendance"
+                    name="attendance"
+                    value={true}
+                    className="cursor-pointer"
+                  />
+                  <label
+                    htmlFor="attendance"
+                    className="text-[16px] leading-[19px] text-['Lato'] font-[400] text-grayLight"
+                  >
+                    Sí
+                  </label>
+                </div>
+
+                <div className="flex items-center gap-[10px]">
+                  <input
+                    type="radio"
+                    ref={register}
+                    error={errors.attendance}
+                    id="attendance2"
+                    name="attendance"
+                    value={false}
+                    className="cursor-pointer"
+                  />
+                  <label
+                    htmlFor="attendance2"
+                    className="text-[16px] leading-[19px] text-['Lato'] font-[400] text-grayLight"
+                  >
+                    No
+                  </label>
+                </div>
               </div>
             </div>
 
